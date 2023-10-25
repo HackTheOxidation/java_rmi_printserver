14f09a969d3c6a84633cc8d8184383d6ec0d52a0
RasmusUK <rasmus.kristensen@live.dk>
2023-10-25 15:27:50 +0200

    Database and login. Updated gitignore

--- src/main/java/printserver/server/Database.java
+++ src/main/java/printserver/server/Database.java
@@ -1,24 +1,59 @@
 package printserver.server;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import javax.security.auth.login.AccountNotFoundException;
+import java.sql.*;
 
 public class Database {
-    static final String URL = "ella.db.elephantsql.com";
-    static final String USER = "efiespng";
-    static final String PASSWORD = "xPT519NpOeC6Y14XZp6vYPoO_IeusQVR";
+    private final String URL = "jdbc:postgresql://ella.db.elephantsql.com:5432/efiespng";
+    private final String USER = "efiespng";
+    private final String PASSWORD = "xPT519NpOeC6Y14XZp6vYPoO_IeusQVR";
 
-    public void createUser(Login login){
-        return;
+    public void createUser(Login login) throws SQLException {
+        try (Connection con = DriverManager.getConnection(URL, USER, PASSWORD)) {
+            PreparedStatement statement = con.prepareStatement(
+                    "INSERT INTO public.\"Logins\"(\n" +
+                    "\t\"Username\", \"PasswordHash\", \"Salt\")\n" +
+                    "\tVALUES (?, ?, ?);");
+            statement.setString(1, login.getUsername());
+            statement.setString(2, login.getPasswordHash());
+            statement.setString(3, login.getSalt());
+        }
     }
 
-    public void changePassword(Login login){
-        return;
+    public void changePassword(Login login) throws AccountNotFoundException {
+        if(login.isAuthenticated()){
+            try (Connection con = DriverManager.getConnection(URL, USER, PASSWORD)) {
+                PreparedStatement statement = con.prepareStatement(
+                        "UPDATE public.\"Logins\"\n" +
+                                "\tSET \"PasswordHash\"=?, \"Salt\"=?\n" +
+                                "\tWHERE \"Username\"=?;");
+                statement.setString(1, login.getUsername());
+                statement.setString(2, login.getPasswordHash());
+                statement.setString(3, login.getSalt());
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }else{
+            throw new AccountNotFoundException();
+        }
     }
 
-    public Login getUser(String username){
-        return new Login();
+    public Login getUser(Login login) throws AccountNotFoundException {
+        try (Connection con = DriverManager.getConnection(URL, USER, PASSWORD)) {
+            PreparedStatement statement = con.prepareStatement(
+                    "SELECT \"Username\", \"PasswordHash\", \"Salt\"\n" +
+                            "\tFROM public.\"Logins\"\n" +
+                            "\tWHERE \"Username\"=?;");
+            statement.setString(1, login.getUsername());
+            try (ResultSet resultSet = statement.executeQuery()) {
+                resultSet.next();
+                return new Login(resultSet.getString("Username"),
+                        resultSet.getString("PasswordHash"),
+                        resultSet.getString("Salt"),
+                        false);
+            }
+        } catch (SQLException e) {
+            throw new AccountNotFoundException();
+        }
     }
 }
--- src/main/java/printserver/server/Login.java
+++ src/main/java/printserver/server/Login.java
@@ -1,7 +1,44 @@
 package printserver.server;
 
 public class Login {
-    public String Username;
-    public String PasswordHash;
-    public String Salt;
+    private final String Username;
+    private final String PasswordHash;
+    private final String Salt;
+    private final boolean Authenticated;
+    public Login(String username, String passwordHash, String salt, boolean authenticated) {
+        Username = username;
+        PasswordHash = passwordHash;
+        Salt = salt;
+        Authenticated = authenticated;
+    }
+
+    public Login(String username, String passwordHash, String salt) {
+        Username = username;
+        PasswordHash = passwordHash;
+        Salt = salt;
+        Authenticated = false;
+    }
+
+    public Login(String username) {
+        Username = username;
+        PasswordHash = null;
+        Salt = null;
+        Authenticated = false;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public String getPasswordHash() {
+        return PasswordHash;
+    }
+
+    public String getSalt() {
+        return Salt;
+    }
+
+    public boolean isAuthenticated() {
+        return Authenticated;
+    }
 }
